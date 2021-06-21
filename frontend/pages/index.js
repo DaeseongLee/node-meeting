a4a20e1f327ec45d62f2ccc82f60c580f3e2390e
DaeseongLee <hsl5539@gmail.com>
2021-06-21 21:46:37 +0900

    Create index.js in pages

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+//import React from 'react';  
+//next는 React를  import할 필요 없다.NEXT가 pages안에 있는 page를 code split 해준다(폴더가 반드시 pages이어야 한다.)
+
+const Home = () => {
+    return (
+        <div>Hello, Next!</div>
+    )
+}
+
+export default Home;
