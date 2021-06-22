a1cc06dd6df578ba221432eb1c4d9b7df5a7a621
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-06-22 20:46:38 +0900

    Add Button Component

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface IProps {
+    canClick: boolean;
+    loading: boolean;
+    actionText: string;
+}
+
+const Button: React.FC<IProps> = ({ canClick, loading, actionText }) => {
+
+    return (
+        <button className={`text-lg font-medium focus:outline-none text-white py-4  transition-colors ${canClick
+            ? "bg-red-600 hover:bg-red-700"
+            : "bg-gray-300 pointer-events-none"
+            }`}>
+            {loading ? "Loading..." : actionText}
+        </button>
+    );
+};
+
+export default Button;
